809c90e6273c0434f73cb46d6698067a8564d892
krausefire3 <69642157+krausefire3@users.noreply.github.com>
2020-10-03 14:00:06 -0400

    10/3

---
+++ bootcamp-prep-master/workshops/02-loops/03-bacteria-time/bacteria-time.js
@@ -0,0 +1,10 @@
+// YOUR CODE BELOW
+function bacteriaTime(currentNum, targetNum) {
+    let currentTime = 0;
+    for (let i = 0; i < targetNum; i++) {
+        currentNum *= 2
+    } 
+return currentNum
+}
+
+bacteriaTime(1, 8)
